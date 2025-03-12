b8128ec4d6bf2c57b87b25f52ec959cdfa4ba9a9
Velvet Thunder <velvet.thunder49@proton.me>
2025-03-12 17:07:11 +0700

    feat: fix

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,13 +1,35 @@
 import { ReactNode } from "react";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { EnvVariables } from "@frontend/components/env.variables.context";
 import { FetchProviderComponent } from "@frontend/hooks/use-fetch";
 import { UserWrapper } from "@frontend/hooks/use-user";
-import { Sidebar } from "@frontend/components/sidebar";
-import { Header } from "@frontend/components/header";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = localFont({
+  src: [
+    {
+      path: '../../public/fonts/inter/Inter-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/inter/Inter-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/inter/Inter-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/inter/Inter-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-inter',
+});
 
 export const metadata = {
   title: "Genius DEX - Trading Bot Platform",
